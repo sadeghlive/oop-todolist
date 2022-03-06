6d792737fd22dbabf3e7131fc614a49ec2197e8d
sadegh <live.sadegh@gmail.com>
2022-03-06 16:03:14 +0330

    start project from zero

--- app.js
+++ app.js
@@ -1,44 +1,26 @@
-class Task {
-    constructor(text) {
+class Task{
+    constructor(text){
         this.text = text
         this.isComplete = false
     }
 }
 
-class Tasks {
-    constructor() {
-        this.grid = document.querySelector('.grid')
-        this.tasks = JSON.parse(localStorage.getItem('tasks')) || []
-        this.render(this.tasks)
+class Tasks{
+    constructor(){
+        this.inputPlace = document.querySelector('#input')
+        this.render()
     }
-    render(chosenArray) {
-        this.htmlInput(chosenArray)
+    render(){
+        this.insertHTML()
     }
-    htmlInput(chosenArray){
-        for(let x = 0; x <=2 ; x++){
-            const div = document.createElement('div')
-            this.grid.append(div)
-        }
-        const input = document.createElement('input')
-        const inputPlace = this.grid.children[1]
-        input.placeholder = 'add or search task'
-        const button = document.createElement('button')
-              button.textContent = 'add new Task!'
-        inputPlace.append(input)
-        inputPlace.append(button)
-
+    insertHTML(){
+        const input = document.createElement('input'),
+              button = document.createElement('button')
+        input.placeholder = 'add/search todo'
+        button.textContent = 'add task to do !'
+        this.inputPlace.append(input)
+        this.inputPlace.append(button)
     }
-
 }
 
-
-
-
-
-
-
-
-
-
-
-let tasks = new Tasks()
+new Tasks()
